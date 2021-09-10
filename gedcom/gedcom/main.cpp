1257fd0f5cd8e2b8d0a9bc8858c27532830cc511
ryanennns <ryanennns@gmail.com>
2021-09-10 15:50:39 -0400

    Creation of the GEDtag class

--- gedcom/gedcom/main.cpp
+++ gedcom/gedcom/main.cpp
@@ -14,12 +14,19 @@
 
 using namespace std;
 
+class GEDtag
+{
+public:
+	std::string layer; //?????
+	std::string tag;
+	std::string value;
+};
+
 class Record
 {
 private:
 	int id;
-	std::string fname;
-	std::string lname;
+	std::string name;
 	bool sex; //true = man, false - woman
 
 	//Date birth;
@@ -55,7 +62,38 @@ public:
 	{
 		recordLines.clear();
 	}
+
+	void parse()
+	{
+		if (!(recordLines.size() == 0))
+		{
+			this->name = recordLines[2];
+			cout << this->name;
+		}
+	}
 };
+
+GEDtag parseLine(const char* f)
+{
+	GEDtag returnValue;
+	char lvl = '\0';
+	char tag[5] = "\0";
+	int i = 0;
+	lvl = f[i];
+	char val[255] = "\0";
+	for (i = 2; f[i] != NULL && f[i] != ' ' && f[i] != '\n'; i++)
+	{   
+		tag[i - 2] = f[i];
+	}
+	while (f[i] != '\0')
+	{
+		val[i] = f[i];
+		i++;
+	}
+	cout << lvl << endl;
+	cout << tag << endl;
+	return returnValue;
+}
 
 
 //MEMORY ACCESS ERRORS EVERYWHERE!
@@ -65,7 +103,7 @@ void readGEDRecord(FILE* f, vector<Record>& records)
 	int i = 0;
 	int recIndex = 0;
 	Record addRecord;
-	while (fgets(tempString, 50, f) != NULL && recIndex < 100)
+	while (fgets(tempString, 50, f) != NULL && recIndex < 3)
 	{
 		if (tempString[0] == '0' && !(i == 0))
 		{
@@ -89,6 +127,8 @@ int main(void)
 	FILE* ged = fopen("gm2107.GED", "r");
 	vector<Record> records;
 
+
+
 	if (ged == nullptr)
 	{
 		cout << "ged is NULL";
@@ -100,13 +140,17 @@ int main(void)
 
 		cout << "Size: " << records.size() << "\n";
 
-		for (int i = 0; i < records.size(); i++)
-		{
-			cout << "\n\nRECORD #" << i << "\n";
-			for (int j = 0; j < records[i].recordLines.size(); j++)
-			{
-				cout << records[i].recordLines[j];
-			}
-		}
+		parseLine(records[0].recordLines[0].c_str());
+
+		//for (int i = 0; i < records.size(); i++)
+		//{
+		//	cout << "\n\nRECORD #" << i + 1 << "\n";
+		//	for (int j = 0; j < records[i].recordLines.size(); j++)
+		//	{
+		//		cout << records[i].recordLines[j];
+		//	}
+		//}
 	}
+
+	return 0;
 }
